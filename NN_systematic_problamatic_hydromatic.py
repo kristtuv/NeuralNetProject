938c7ad84849f55de43dae9b92666f9e1587854a
Kristian Tuv <ktuv@hotmail.com>
2018-11-08 15:40:42 +0100

    Looping over sample sizes

--- NN_systematic_problamatic_hydromatic.py
+++ NN_systematic_problamatic_hydromatic.py
@@ -9,8 +9,9 @@ def plot_regularization():
     ###############Defining parameters#####################
     #######################################################
     lambs = np.logspace(-4, 0, 8)
-    n_samples = 10000
-    
+    # n_samples =[100, 1000, 4000, 10000]
+    n_samples = [40000]
+
     #######################################################
     ###############Fetching Data###########################
     #######################################################
@@ -18,127 +19,129 @@ def plot_regularization():
     X, Y = shuffle(X, Y)
     X_critical, Y_critical = shuffle(X_critical, Y_critical)
 
-    X_train = X[:n_samples]; Y_train = Y[:n_samples]
-    X_test = X[n_samples:2*n_samples]; Y_test = Y[n_samples:2*n_samples]
-    X_crit = X_critical[:n_samples]; Y_crit = Y_critical[:n_samples]
-    
-   
-
-    #######################################################
-    ###########Dictionaries for ploting####################
-    #######################################################
-    errors = {'ols': {'Train': [], 'Test': [], 'Crit': []}, 
-              'l1':  {'Train': [], 'Test': [], 'Crit': []},
-              'l2':  {'Train': [], 'Test': [], 'Crit': []}}
 
 
-    accuracies = {'ols': {'Train': [], 'Test': [], 'Crit': []}, 
-              'l1':  {'Train': [], 'Test': [], 'Crit': []},
-              'l2':  {'Train': [], 'Test': [], 'Crit': []}}
-    
+    for sample in n_samples:
+        X_train = X[:sample]; Y_train = Y[:sample]
+        X_test = X[sample:2*sample]; Y_test = Y[sample:2*sample]
+        X_crit = X_critical[:sample]; Y_crit = Y_critical[:sample]
 
-    for lamb in lambs:
-        #######################################################
-        ###########Initializing networks#######################
-        #######################################################
-        nn = NeuralNet( X_train, Y_train, nodes = [X.shape[1],  10, 2], 
-                activations = ['sigmoid', None], cost_func='log')
-        nn_l1 = NeuralNet( X_train, Y_train, nodes = [X.shape[1],  10, 2], 
-                activations = ['sigmoid', None], cost_func='log', regularization='l1', lamb=lamb)
-        nn_l2 = NeuralNet( X_train, Y_train, nodes = [X.shape[1],  10, 2], 
-                activations = ['sigmoid', None], cost_func='log', regularization='l2', lamb=lamb)
 
         #######################################################
-        ###########Spliting data#######################
+        ###########Dictionaries for ploting####################
         #######################################################
-        nn.split_data(frac=0.5, shuffle=True)
-        nn_l1.split_data(frac=0.5, shuffle=True)
-        nn_l2.split_data(frac=0.5, shuffle=True)
-        
-        #######################################################
-        ###########Training network#######################
-        #######################################################
-
-        nn.TrainNN(epochs = 250, eta0 = 0.05, n_print=250)
-        nn_l1.TrainNN(epochs = 250, eta0 = 0.05, n_print=250)
-        nn_l2.TrainNN(epochs = 250, eta0 = 0.05, n_print=250)
-       
-         
-        #######################################################
-        ###########Error and accuracies ols#######################
-        #######################################################
-        ypred_train = nn.feed_forward(X_train, isTraining=False)
-        ypred_test = nn.feed_forward(X_test, isTraining=False)
-        ypred_crit = nn.feed_forward(X_crit, isTraining=False)
-        errors['ols']['Train'].append(nn.cost_function(Y_train, ypred_train))
-        errors['ols']['Test'].append(nn.cost_function(Y_test, ypred_test))
-        errors['ols']['Crit'].append(nn.cost_function(Y_crit, ypred_crit))
-        accuracies['ols']['Train'].append(nn.accuracy(Y_train, ypred_train))
-        accuracies['ols']['Test'].append(nn.accuracy(Y_test, ypred_test))
-        accuracies['ols']['Crit'].append(nn.accuracy(Y_crit, ypred_crit))
-
-        #######################################################
-        ###########Error and accuracies l1#######################
-        #######################################################
-        ypred_train = nn_l1.feed_forward(X_train, isTraining=False)
-        ypred_test = nn_l1.feed_forward(X_test, isTraining=False)
-        ypred_crit = nn_l1.feed_forward(X_crit, isTraining=False)
-        errors['l1']['Train'].append(nn_l1.cost_function(Y_train, ypred_train))
-        errors['l1']['Test'].append(nn_l1.cost_function(Y_test, ypred_test))
-        errors['l1']['Crit'].append(nn_l1.cost_function(Y_crit, ypred_crit))
-        accuracies['l1']['Train'].append(nn_l1.accuracy(Y_train, ypred_train))
-        accuracies['l1']['Test'].append(nn_l1.accuracy(Y_test, ypred_test))
-        accuracies['l1']['Crit'].append(nn_l1.accuracy(Y_crit, ypred_crit))
-
-        #######################################################
-        ###########Error and accuracies l2#######################
-        #######################################################
-        ypred_train = nn_l2.feed_forward(X_train, isTraining=False)
-        ypred_test = nn_l2.feed_forward(X_test, isTraining=False)
-        ypred_crit = nn_l2.feed_forward(X_crit, isTraining=False)
-        errors['l2']['Train'].append(nn_l2.cost_function(Y_train, ypred_train))
-        errors['l2']['Test'].append(nn_l2.cost_function(Y_test, ypred_test))
-        errors['l2']['Crit'].append(nn_l2.cost_function(Y_crit, ypred_crit))
-        accuracies['l2']['Train'].append(nn_l2.accuracy(Y_train, ypred_train))
-        accuracies['l2']['Test'].append(nn_l2.accuracy(Y_test, ypred_test))
-        accuracies['l2']['Crit'].append(nn_l2.accuracy(Y_crit, ypred_crit))
+        errors = {'ols': {'Train': [], 'Test': [], 'Crit': []},
+                  'l1':  {'Train': [], 'Test': [], 'Crit': []},
+                  'l2':  {'Train': [], 'Test': [], 'Crit': []}}
 
 
-    datasetnames = ['Train', 'Test']
-    errfig, errax = plt.subplots()
-    accfig, accax = plt.subplots()
-    colors = ['#1f77b4', '#ff7f0e', '#2ca02c']
-    linestyles = ['-', '--']
-    for i, key in enumerate(accuracies):
-        for j, name in enumerate(datasetnames):
-            errax.set_xlabel(r'$\lambda$')
-            errax.set_ylabel('Error')
-            errax.semilogx(lambs[:-1], errors[str(key)][str(name)][:-1], 
-                    color=colors[i], linestyle=linestyles[j], label=str(key).capitalize()+'_'+str(name))
-            errax.legend()
+        accuracies = {'ols': {'Train': [], 'Test': [], 'Crit': []},
+                  'l1':  {'Train': [], 'Test': [], 'Crit': []},
+                  'l2':  {'Train': [], 'Test': [], 'Crit': []}}
+
+
+        for lamb in lambs:
+            #######################################################
+            ###########Initializing networks#######################
+            #######################################################
+            nn = NeuralNet( X_train, Y_train, nodes = [X.shape[1],  10, 2],
+                    activations = ['sigmoid', None], cost_func='log')
+            nn_l1 = NeuralNet( X_train, Y_train, nodes = [X.shape[1],  10, 2],
+                    activations = ['sigmoid', None], cost_func='log', regularization='l1', lamb=lamb)
+            nn_l2 = NeuralNet( X_train, Y_train, nodes = [X.shape[1],  10, 2],
+                    activations = ['sigmoid', None], cost_func='log', regularization='l2', lamb=lamb)
+
+            #######################################################
+            ###########Spliting data#######################
+            #######################################################
+            nn.split_data(frac=0.5, shuffle=True)
+            nn_l1.split_data(frac=0.5, shuffle=True)
+            nn_l2.split_data(frac=0.5, shuffle=True)
+
+            #######################################################
+            ###########Training network#######################
+            #######################################################
+
+            nn.TrainNN(epochs = 250, eta0 = 0.05, n_print=250)
+            nn_l1.TrainNN(epochs = 250, eta0 = 0.05, n_print=250)
+            nn_l2.TrainNN(epochs = 250, eta0 = 0.05, n_print=250)
+
+
+            #######################################################
+            ###########Error and accuracies ols#######################
+            #######################################################
+            ypred_train = nn.feed_forward(X_train, isTraining=False)
+            ypred_test = nn.feed_forward(X_test, isTraining=False)
+            ypred_crit = nn.feed_forward(X_crit, isTraining=False)
+            errors['ols']['Train'].append(nn.cost_function(Y_train, ypred_train))
+            errors['ols']['Test'].append(nn.cost_function(Y_test, ypred_test))
+            errors['ols']['Crit'].append(nn.cost_function(Y_crit, ypred_crit))
+            accuracies['ols']['Train'].append(nn.accuracy(Y_train, ypred_train))
+            accuracies['ols']['Test'].append(nn.accuracy(Y_test, ypred_test))
+            accuracies['ols']['Crit'].append(nn.accuracy(Y_crit, ypred_crit))
+
+            #######################################################
+            ###########Error and accuracies l1#######################
+            #######################################################
+            ypred_train = nn_l1.feed_forward(X_train, isTraining=False)
+            ypred_test = nn_l1.feed_forward(X_test, isTraining=False)
+            ypred_crit = nn_l1.feed_forward(X_crit, isTraining=False)
+            errors['l1']['Train'].append(nn_l1.cost_function(Y_train, ypred_train))
+            errors['l1']['Test'].append(nn_l1.cost_function(Y_test, ypred_test))
+            errors['l1']['Crit'].append(nn_l1.cost_function(Y_crit, ypred_crit))
+            accuracies['l1']['Train'].append(nn_l1.accuracy(Y_train, ypred_train))
+            accuracies['l1']['Test'].append(nn_l1.accuracy(Y_test, ypred_test))
+            accuracies['l1']['Crit'].append(nn_l1.accuracy(Y_crit, ypred_crit))
+
+            #######################################################
+            ###########Error and accuracies l2#######################
+            #######################################################
+            ypred_train = nn_l2.feed_forward(X_train, isTraining=False)
+            ypred_test = nn_l2.feed_forward(X_test, isTraining=False)
+            ypred_crit = nn_l2.feed_forward(X_crit, isTraining=False)
+            errors['l2']['Train'].append(nn_l2.cost_function(Y_train, ypred_train))
+            errors['l2']['Test'].append(nn_l2.cost_function(Y_test, ypred_test))
+            errors['l2']['Crit'].append(nn_l2.cost_function(Y_crit, ypred_crit))
+            accuracies['l2']['Train'].append(nn_l2.accuracy(Y_train, ypred_train))
+            accuracies['l2']['Test'].append(nn_l2.accuracy(Y_test, ypred_test))
+            accuracies['l2']['Crit'].append(nn_l2.accuracy(Y_crit, ypred_crit))
+
+
+        datasetnames = ['Train', 'Test']
+        errfig, errax = plt.subplots()
+        accfig, accax = plt.subplots()
+        colors = ['#1f77b4', '#ff7f0e', '#2ca02c']
+        linestyles = ['-', '--']
+        for i, key in enumerate(accuracies):
+            for j, name in enumerate(datasetnames):
+                errax.set_xlabel(r'$\lambda$')
+                errax.set_ylabel('Error')
+                errax.semilogx(lambs[:-2], errors[str(key)][str(name)][:-2],
+                        color=colors[i], linestyle=linestyles[j], label=str(key).capitalize()+'_'+str(name))
+                errax.legend()
 
 
 
-            accax.set_xlabel(r'$\lambda$')
-            accax.set_ylabel('Accuracy')
-            accax.semilogx(lambs, accuracies[str(key)][str(name)],
-                    color=colors[i], linestyle=linestyles[j], label=str(key).capitalize()+'_'+str(name))
-            accax.legend()
+                accax.set_xlabel(r'$\lambda$')
+                accax.set_ylabel('Accuracy')
+                accax.semilogx(lambs, accuracies[str(key)][str(name)],
+                        color=colors[i], linestyle=linestyles[j], label=str(key).capitalize()+'_'+str(name))
+                accax.legend()
 
 
 
 
-    critfig, critax = plt.subplots()
-    critax.set_xlabel(r'$\lambda$')
-    critax.set_ylabel('Accuracy')
-    for i, key in enumerate(accuracies):
-        critax.semilogx(lambs, accuracies[str(key)]['Crit'],
-             label=str(key).capitalize()+'_Crit')
-        critax.legend()
+        critfig, critax = plt.subplots()
+        critax.set_xlabel(r'$\lambda$')
+        critax.set_ylabel('Accuracy')
+        for i, key in enumerate(accuracies):
+            critax.semilogx(lambs, accuracies[str(key)]['Crit'],
+                 label=str(key).capitalize()+'_Crit')
+            critax.legend()
 
-    errfig.savefig('error'+str(n_samples)+'.png') 
-    accfig.savefig('accuracy'+str(n_samples)+'.png')
-    critfig.savefig('crit'+str(n_samples)+'.png')
+        errfig.savefig('error'+str(sample)+'.png')
+        accfig.savefig('accuracy'+str(sample)+'.png')
+        critfig.savefig('crit'+str(sample)+'.png')
 
 
 def check_regularization():
@@ -149,13 +152,13 @@ def check_regularization():
     lambs = [0.00001, 0.0001, 0.0008, 0.0009, 0.001, 0.002, 0.01, 0.1, 0.5]
     for i in range(1,2):
         X = X[:i*5000]; Y = Y[:i*5000]
-        nn = NeuralNet( X, Y, nodes = [X.shape[1],  10, 2], 
+        nn = NeuralNet( X, Y, nodes = [X.shape[1],  10, 2],
                 activations = ['sigmoid', None], cost_func='log')
 
         for lamb in lambs:
-            nn_l1 = NeuralNet( X, Y, nodes = [X.shape[1],  10, 2], 
+            nn_l1 = NeuralNet( X, Y, nodes = [X.shape[1],  10, 2],
                     activations = ['sigmoid', None], cost_func='log', regularization='l1', lamb=lamb)
-            nn_l2 = NeuralNet( X, Y, nodes = [X.shape[1],  10, 2], 
+            nn_l2 = NeuralNet( X, Y, nodes = [X.shape[1],  10, 2],
                     activations = ['sigmoid', None], cost_func='log', regularization='l2', lamb=lamb)
             nn.split_data(frac=0.5, shuffle=True)
             nn_l1.split_data(frac=0.5, shuffle=True)
@@ -195,13 +198,13 @@ def best_regularization():
     #####################
     ###Neural Net #######
     #####################
-    nn_l1 = NeuralNet( X, Y, nodes = [X.shape[1], 10,  10, 2], 
+    nn_l1 = NeuralNet( X, Y, nodes = [X.shape[1], 10,  10, 2],
             activations = ['sigmoid', None], cost_func='log', regularization='l1', lamb=0.002)
     nn_l1.split_data(frac=0.5, shuffle=True)
 
 
 
-    # nn_l2 = NeuralNet( X, Y, nodes = [X.shape[1],  10, 2], 
+    # nn_l2 = NeuralNet( X, Y, nodes = [X.shape[1],  10, 2],
     #         activations = ['sigmoid', None], cost_func='log', regularization='l2', lamb=0.1)
     # nn_l2.split_data(frac=0.5, shuffle=True)
     print("#####L1#####")
@@ -221,7 +224,7 @@ def best_regularization():
     # critError = nn_l2.cst_function(Y_critical, ypred_crit)
     # critAcc = nn_l2.accuracy(Y_critcal, ypred_crit)
 
-    print("Critical error: %g,  Critical accuracy: %g" %(critError, critAcc)) 
+    print("Critical error: %g,  Critical accuracy: %g" %(critError, critAcc))
 
 
 
@@ -245,9 +248,9 @@ exit()
 # #      permutations.append(p + [None])
 # #      permutations.append(p[:-1] + [None])
 # #      permutations.append(p[:-2] + [None])
-    
+
 # #  unique_activations = [list(x) for x in set(tuple(x) for x in permutations)]
-# #  unique_activations = sorted(unique_activations, key=len) 
+# #  unique_activations = sorted(unique_activations, key=len)
 
 
 #  def architecture(activations = ['sigmoid', 'tanh', 'relu'], n_hidden_layers=2):
@@ -256,29 +259,29 @@ exit()
 #          for activation in activations:
 #              acts.append([ activation ]*hl + [None])
 #      return acts
-          
+
 #  if __name__ == '__main__':
-    
+
 #      regularizations = ['l1', 'l2']
 #      activations = activation_list()
 #      learning_rates =  np.logspace(-3, 2, 10)
-#      print(activations)     
-    
-    
+#      print(activations)
+
+
 #      exit()
 
 #  cost_functions = ['log', 'mse']
 #  learning_rates = [0.001, 0.01, 0.1, 1, 10]
-#  nodes = [X.shape[1], 
+#  nodes = [X.shape[1],
 
 
 
-#  # _     _       _     _     _       _     
-# # | |__ | | __ _| |__ | |__ | | __ _| |__  
-# # | '_ \| |/ _` | '_ \| '_ \| |/ _` | '_ \ 
+#  # _     _       _     _     _       _
+# # | |__ | | __ _| |__ | |__ | | __ _| |__
+# # | '_ \| |/ _` | '_ \| '_ \| |/ _` | '_ \
 # # | |_) | | (_| | | | | |_) | | (_| | | | |
 # # |_.__/|_|\__,_|_| |_|_.__/|_|\__,_|_| |_|
-                                        
+
 
 
 
